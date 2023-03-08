e6888e1f421cb555ab82400fd0e3fb2afa23528e
gogolashvili <ggogolashvili91@gmail.com>
2023-03-08 06:07:41 +0400

    add styles. change html. add j s functions

---
+++ main.js
@@ -0,0 +1,71 @@
+let bill = document.querySelector(".input1");
+let person = document.querySelector(".input2");
+let custom = document.querySelector(".inppro");
+let amount = document.querySelector("h3");
+let total = document.querySelector("h4");
+let reset = document.querySelector(".reset");
+let percent = document.querySelector("button");
+
+let billInput = 0;
+let customInput = 0;
+let personInput = 0;
+
+function calculateTip() {
+  if (billInput && personInput) {
+    let tipPercent = 0;
+    if (customInput) {
+      tipPercent = customInput;
+    } else {
+      tipPercent = parseFloat(amount.innerHTML);
+    }
+    let amountValue = (billInput * (tipPercent / 100)) / personInput;
+    let totalValue = billInput / personInput + amountValue;
+    amount.innerHTML = "$" + amountValue.toFixed(2);
+    total.innerHTML = "$" + totalValue.toFixed(2);
+  }
+}
+
+bill.addEventListener("input", function () {
+  firstInput = bill.value;
+  calculateTip();
+});
+
+custom.addEventListener("input", function () {
+  customInput = custom.value;
+  calculateTip();
+});
+
+person.addEventListener("input", function () {
+  personInput = person.value;
+  calculateTip();
+});
+
+reset.addEventListener("click", function () {
+  billInput = 0;
+  customInput = 0;
+  personInput = 0;
+  bill.value = "";
+  custom.value = "";
+  person.value = "";
+  amount.innerHTML = "$0.00";
+  total.innerHTML = "$0.00";
+});
+
+let some;
+percent.forEach((x) => {
+  x.addEventListener("click", function () {
+    some = x.innerHTML;
+    amount.innerHTML = x.innerHTML;
+    total.innerHTML = x.innerHTML;
+    custom.value = "";
+    customInput = 0;
+    calculateTip();
+    x.className = "style";
+
+    percent.forEach((y) => {
+      if (y.innerHTML !== some) {
+        y.className = "button";
+      }
+    });
+  });
+});
